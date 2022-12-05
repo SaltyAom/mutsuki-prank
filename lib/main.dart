5df37e21ff39e8a941562645841b1f4a7f803261
saltyaom <saltyaom@gmail.com>
2022-12-06 00:39:56 +0700

    :wrench: fix: add cunny.mp3

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
@@ -34,7 +35,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Card Senpai',
+      title: 'Mutsuki Prank',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         useMaterial3: true,
@@ -125,6 +126,7 @@ class IWannaKnowOWO extends HookWidget {
             ..center
             ..scrollable
             ..expanded
+            ..maxW = 332
         ])
           ..center
           ..gap = 32,
@@ -148,7 +150,11 @@ class ArigatouSensei extends StatelessWidget {
         n.Button(
             "Hmm ... Damn female kid ...! !! !! Pranking an adult 💢💢 I need correction ...💢"
                 .n)
-          ..onPressed = () {
+          ..onPressed = () async {
+            await AudioPlayer().play(AssetSource("cunny.mp3"));
+
+            await Future.delayed(const Duration(milliseconds: 4300));
+
             exit(0);
           }
           ..useParent(
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,12 @@
 import FlutterMacOS
 import Foundation
 
+import audioplayers_darwin
 import bitsdojo_window_macos
+import path_provider_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
   BitsdojoWindowPlugin.register(with: registry.registrar(forPlugin: "BitsdojoWindowPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
